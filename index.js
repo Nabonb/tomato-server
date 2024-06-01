bb31e37e4d6055a57b844078943edcacf6213851
Pratham Barua Nabo <nabobarua2@gmail.com>
2024-06-01 22:12:19 +0600

    MongoDB integrated done

--- index.js
+++ index.js
@@ -28,9 +28,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // const usersCollection = client.db('aircncDb').collection('users')
+    const usersCollection = client.db('tomatoDB').collection('users')
     // const roomsCollection = client.db('aircncDb').collection('rooms')
     // const bookingsCollection = client.db('aircncDb').collection('bookings')
+
+    //Save user email and role into the mongodb userCollection 
+    app.put('/users/:email',async(req,res)=>{
+      const email = req.params.email
+      const user = req.body
+      const query = {email: email }
+      const options = {upsert : true}
+      const updateDoc = {
+        $set : user
+      }
+      const result = await usersCollection.updateOne(query,updateDoc,options)
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
